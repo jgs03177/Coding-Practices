c09f099dba24f2332bcf2db992f472d7e731769b
jgs03177 <jgs03177@gmail.com>
2022-07-25 02:33:54 +0900

    template update

--- template_atcoder.py
+++ template_atcoder.py
@@ -1,6 +1,8 @@
 import sys
-input=sys.stdin.readline
 
-n=int(input())
-s=input().rstrip()
-*w,=map(int,input().split())
+input = sys.stdin.readline
+
+h, w = map(int, input().split())
+n = int(input())
+s = input().rstrip()
+*l, = map(int, input().split())
--- template_codeforces.py
+++ template_codeforces.py
@@ -1,10 +1,11 @@
 import sys
-input=sys.stdin.readline
 
-t=int(input())
+input = sys.stdin.readline
+
+t = int(input())
 
 for _ in range(t):
-    h,w=map(int,input().split())
-    n=int(input())
-    s=input().rstrip()
-    *l,=map(int,input().split())
+    h, w = map(int, input().split())
+    n = int(input())
+    s = input().rstrip()
+    *l, = map(int, input().split())
